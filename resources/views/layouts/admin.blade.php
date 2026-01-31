7ac69c039cb6b093eee33a2b8fb473852554b83d
Bruno-Boschi <brunoboschi72@gmail.com>
2026-01-31 12:32:25 -0300

    install ADMINLTE and Breeze

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>@yield('header')</h1>
+@endsection
+
+@section('content')
+    @yield('content')
+@endsection
